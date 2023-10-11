e26fddb5198791a858b1edc2d7b35345c74f7dca
Yusuke Sakai <70700401+yusuke-dna@users.noreply.github.com>
2023-10-12 03:08:15 +0900

    Update auto-journal-club-server.py

--- auto-journal-club-server.py
+++ auto-journal-club-server.py
@@ -3,20 +3,21 @@ import requests
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 import openai
+import re
 
 # Inputs through command line arguments
 parser = argparse.ArgumentParser(description="Process and analyze a given URL and text.")
 parser.add_argument('--openai_key', required=True, help="The API key for OpenAI.")
 parser.add_argument('--webhook_url', required=True, help="The webhook endpoint URL.")
-parser.add_argument('--url', required=True, help="The URL to be processed.")
-parser.add_argument('--text', required=True, help="The title or text associated with the URL.")
+parser.add_argument('--source', required=True, help="The source account.")
+parser.add_argument('--text', required=True, help="The title and URL.")
 args = parser.parse_args()
 
 # Use the parsed arguments
 openai.api_key = args.openai_key
 SLACK_WEBHOOK = args.webhook_url
 
-def handle_chain_of_events(url, text):
+def handle_chain_of_events(url, text, source):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -26,26 +27,28 @@ def handle_chain_of_events(url, text):
         if not cleaned_content:
             print("Error: Couldn't extract content from the page.")
             return
-
+        print(cleaned_content)
         openai_response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
             messages=[
                 {
                     "role": "system",
-                    "content": "You are a helpful assistant, with high quality expertise in molecular biology, biochemistry, and nanotechnology. The response should be in Japanese and following format: \n\n**Title:[English title]** \n\n**Authors:**[Authors list with short affiliation (university or institute name and country)] \n\n**Abstract:**[Japanese translation of the abstract] \n\n**要点:**[Japanese translation of the important background and key messages of the research]"
+                    "content": "You are a helpful assistant, with high quality expertise in molecular biology, biochemistry, and nanotechnology. The response should be in Japanese and following format: \n\n**Title:[English title]** \n\n**Authors:**[Authors list with short affiliation (university or institute name and country)] \n\n**Abstract:**[日本語翻訳されたabstract] \n\n**要点:**[Japanese translation of the important background and key messages of the research]"
                 },
                 {
                     "role": "user",
-                    "content": f"Following document is an article of an academic journal titled:{text}. It is extracted from web page HTML by beautiful soup 4. Please find the abstract of the article, author names, and the affiliations of the individual authors from the content and then translate the abstract to Japanese. At last of the response, please add, in Japanese, important background and key messages of the research to help the reader grasp the research contents. \n\n{cleaned_content}"
+                    "content": f"このドキュメントは学術論文の記事で、そのタイトルとURLは{text} です。このドキュメントはWeb PageのHTMLから抽出され、Beautifl Soup4 によって整形されました。ドキュメント中からAbstractを抽出して日本語翻訳してください。また、タイトルと著者名、所属を冒頭に記してください。レスポンスの最後には重要な背景情報とキーメッセージを箇条書きを付すことで、ひとめ見て研究内容が理解できるように工夫してください。 \n\n{cleaned_content}"
                 }
             ]
         )
         assistant_message = openai_response['choices'][0]['message']['content']
 
+        print(assistant_message)
+
         headers_for_slack = {'Content-Type': 'application/json'}
         payload = {
             "text": assistant_message,
-            "username": "MyBot",
+            "username": source,
             "icon_emoji": ":robot_face:"
         }
         requests.post(SLACK_WEBHOOK, json=payload, headers=headers_for_slack)
@@ -54,7 +57,13 @@ def handle_chain_of_events(url, text):
         print(f"Error: {e}")
 
 if __name__ == '__main__':
-    handle_chain_of_events(args.url, args.text)
+    url_pattern = re.compile(
+        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    )
+    urls = re.findall(url_pattern, args.text)
+    print(urls[0])
+
+    handle_chain_of_events(urls[0], args.text, args.source)
 else:
     headers_for_slack = {'Content-Type': 'application/json'}
     payload = {
